f799b31bafb5e4efaed39b246306f1ececb1c3fb
profsibz3090 <sibandadonty@gmail.com>
2025-02-05 13:21:25 +0200

    all events routes added

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FilterEvents() {
+  return (
+    <div>
+      <h1>Filter Events</h1>
+    </div>
+  )
+}
+
+export default FilterEvents
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StartingPage() {
+  return (
+    <div>
+      <h>Starting Page</h>
+    </div>
+  )
+}
+
+export default StartingPage
