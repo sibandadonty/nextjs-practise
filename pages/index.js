84b6bec6a2f7224192713f2c3d111edbd858d966
profsibz3090 <sibandadonty@gmail.com>
2025-02-24 10:41:16 +0200

    added getStaticProps in main file

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-function HomePage() {
+function HomePage(props) {
+  console.log("Data: ", props.data);
+  
   return (
-    <div>
-      <h1 className='text-4xl'>Hello World</h1>
+    <div className='w-full min-h-screen'>
+      <div className='max-w-500 h-full flex justify-center flex-col'>
+        {props.data.map(item => (
+          <div className='shadow-md p-4 w-100' key={item.id}>
+            <p>Name: {item.name}</p>
+            <p>Username: {item.username}</p>
+            <p>Email: {item.email}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default HomePage
+export const getStaticProps = async (context) => {
+   const result = await fetch("https://jsonplaceholder.typicode.com/users");
+   const data = await result.json();
+
+  return {
+    props: {
+      data
+    }
+  }
+};
+
+export default HomePage;
